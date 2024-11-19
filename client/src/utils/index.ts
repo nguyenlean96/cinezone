c2b63c395d3a1be84d4c4776972bc19451ffdc78
Le An Nguyen <nguyenlean96@gmail.com>
2024-11-19 16:52:28 -0500

    Added util methods

---
+++ client/src/utils/index.ts
@@ -0,0 +1,7 @@
+import fetcher from "./fetcher";
+import * as string_helper from "./string/";
+
+export { 
+  fetcher,
+  string_helper
+};
---
+++ client/src/utils/string/index.ts
@@ -0,0 +1,5 @@
+import stringHashing from "./hasing";
+
+export {
+  stringHashing
+}
